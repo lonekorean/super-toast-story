d94f4381ad4862e00a7f0f1a43ed7d9fe303725e
Will Boyd <will@codersblock.com>
2016-06-13 10:30:38 -0400

    Starting dialog

--- client/site.js
+++ client/site.js
@@ -1,11 +1,10 @@
 $(function() {
-	var $loading = $('.loading');
+	var text1 = 'And so toast flew off into the stars. He never came back...';
+
 	var $getReady = $('.get-ready');
 	var $start = $('.start');
-
-	var $playing = $('.playing');
-
-	$start.on('click', handleStart);
+	var $toast = $('.toast');
+	var $dialog = $('.dialog');
 
 	var music = document.getElementById('music');
 	if (music.readyState === 4) {
@@ -17,11 +16,28 @@ $(function() {
 	function handleReady() {
 		$getReady.hide();
 		$start.show();
+
+		$start.on('click', handleStart);
 	}
 
 	function handleStart() {
-		$loading.hide();
-		$playing.show();
+		$start.hide();
+		$toast.show();
+
 		music.play();
+		setTimeout(startDialog.bind(this, text1), 10000); // wait for toast fade in
+	}
+
+	function startDialog(text) {
+		$dialog.show();
+		setTimeout(continueDialog.bind(this, text), 2000);
+	}
+
+	function continueDialog(text) {
+		$dialog.append(text.charAt(0));
+		var text = text.slice(1);
+		if (text.length > 0) {
+			setTimeout(continueDialog.bind(this, text), 250);
+		}
 	}
 });
